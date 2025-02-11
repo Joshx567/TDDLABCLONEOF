e6846ebe508e692ad8775cd9bed5d5caaddaa327
Jose <jose.lopez.s@ucb.edu.bo>
2025-02-11 12:19:45 -0400

    second commit

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,26 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const form2 = document.querySelector("#multip-form");
 
+const div = document.querySelector("#resultado-div");
+const div2 = document.querySelector("#resultado-multiplicar");
+                                  
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = Number.parseInt(document.querySelector("#primer-numero").value);
+  const secondNumber = Number.parseInt(document.querySelector("#segundo-numero").value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(document.querySelector("#primer-num-multip").value);
+  const secondNumber = Number.parseInt(document.querySelector("#segundo-num-multip").value);
+
+  div2.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
